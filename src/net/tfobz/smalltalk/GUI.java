5eee27c4451474005c064f0a953f5dacc71d8764
Flapp <julian.tschager@gmail.com>
2019-02-13 20:39:05 +0100

    Addaed Pane

--- src/net/tfobz/smalltalk/GUI.java
+++ src/net/tfobz/smalltalk/GUI.java
@@ -1,21 +1,28 @@
 package net.tfobz.smalltalk;
 
-import java.awt.Color;
 import java.awt.geom.Line2D;
 
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
+import javax.swing.JTextPane;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Style;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyledDocument;
 
 import dframe.DButton;
 import dframe.DFrame;
 import dframe.DFrameConstants;
 import dframe.DTextField;
+
 import java.awt.*;
 
 public class GUI extends DFrame {
 
 	private DTextField text;
 	private DButton button;
+	private JTextPane area;
+	private Style style;
 
 	public GUI() {
 		setSize(1600, 900);
@@ -34,12 +41,36 @@ public class GUI extends DFrame {
 
 		try {
 			Image img = ImageIO.read(getClass().getResource("senden.png"));
-			Image newimg = img.getScaledInstance(button.getWidth(), button.getHeight(), java.awt.Image.SCALE_SMOOTH);
+			Image newimg = img.getScaledInstance(button.getWidth(), button.getHeight(), Image.SCALE_SMOOTH);
 			button.setIcon(new ImageIcon(newimg));
 		} catch (Exception ex) {
 			System.out.println(ex);
 		}
 		add(button);
+
+		area = new JTextPane();
+		StyledDocument doc = area.getStyledDocument();
+
+		style = area.addStyle("I'm a Style", null);
+		StyleConstants.setForeground(style, Color.WHITE);
+
+		area.setBounds(0, 0, getWidth(), 810);
+		area.setBackground(new Color(60, 60, 60));
+		area.setForeground(Color.WHITE);
+		area.setEditable(false);
+		area.setFont(new Font("Segoe UI Symbol", Font.PLAIN, 15));
+		add(area);
+
+		// Zin einikritzeln
+		// try {
+		// StyleConstants.setForeground(style, Color.WHITE);
+		// doc.insertString(0, "Scheduler is now running (schedule period = 10)\n",
+		// style);
+		// StyleConstants.setForeground(style, dframe.ColorPalette.BLAU);
+		// doc.insertString(0, "Message: ", style);
+		// } catch (BadLocationException e) {
+		// }
+
 	}
 
 	public void paint(Graphics g) {
